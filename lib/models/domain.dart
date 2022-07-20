e2881b9bfcad012fa2510b14014f807a10277f4b
Omari Warner <lonewalke22@gmail.com>
2022-07-20 12:14:19 -0400

    added a function hello world to domain class

--- lib/models/domain.dart
+++ lib/models/domain.dart
@@ -1,4 +1,4 @@
-class Domain{
+class Domain {
   String name;
 
   Domain(this.name);
@@ -6,8 +6,11 @@ class Domain{
   //The name that is retrieved from the internet has dashes in it.
   // Replace all the dashes with spaces so that the user will see
   // sensible information on the user interface
-  String convertNameToHumanReadableFormat(){
+  String convertNameToHumanReadableFormat() {
     return name.replaceAll("-", " ");
   }
 
+  void hello() {
+    print("hello World");
+  }
 }
