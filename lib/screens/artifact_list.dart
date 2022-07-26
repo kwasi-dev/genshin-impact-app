b7478160f1638e3629d535e8b5e672d0e841d745
Omari Warner <lonewalke22@gmail.com>
2022-07-25 20:26:24 -0400

    changed artifact list

--- lib/screens/artifact_list.dart
+++ lib/screens/artifact_list.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:genshinapp/models/domain.dart';
 import 'package:genshinapp/screens/artifact_detail.dart';
 import 'package:genshinapp/utilities/api.dart';
 import 'package:http/http.dart';
